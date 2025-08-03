651e12613a24348335c22a7733cc92711ea9dbae
mahsa <m.zargandeh85@gmail.com>
2025-08-03 09:11:49 +0330

    Add project base structure and Tailwind styles

--- berench-developer/tailwind.config.js
+++ berench-developer/tailwind.config.js
@@ -1,5 +1,6 @@
 
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,5 +10,6 @@ module.exports = {
   },
   plugins: [],
 }
+
 
 
